0e93c4c843640fc2e392ef1a99874a507a2e7331
mjpelah <97266233+mjpelah@users.noreply.github.com>
2023-03-06 17:19:57 -0500

    Update refre.sh
    Changed the shell script to add a random number of customers and random type of customer

--- Part3/refre.sh
+++ Part3/refre.sh
@@ -6,14 +6,13 @@ sudo insmod barstool.ko
 echo ----- initial state
 cat ../../../../proc/majorsbar
 # a.out 2 (num) (type) to summon customer. 0 freshman, 1 sophomore, 2 junior, 3 senior, 4 prof / grad
-./a.out 2 2 1
+./a.out 2 $RANDOM % 8 $RANDOM % 5
 echo ----- state after 2 sophomore
 cat ../../../../proc/majorsbar
-./a.out 2 4 4
+./a.out 2 $RANDOM % 8 $RANDOM % 5
 echo ----- state after 4 profs
 cat ../../../../proc/majorsbar
 # close bar
 ./a.out 3
 echo ----- closed state
 cat ../../../../proc/majorsbar
-
